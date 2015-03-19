4e8b6872c82a69a0d996d20a05ebdd01657abe63
Ali Zaidi <azaidi@maxeler.com>
2015-03-19 12:50:23 +0000

    Adding ArgumentParser.

--- palal/palal.py
+++ palal/palal.py
@@ -1,5 +1,39 @@
+# -*- coding: utf-8 -*-
+
+"""
+    Palal - Pick A License, Any License!
+    ------------------------------------
+
+    Picking an open-source license can be confusing for seasoned developers
+    let along noobs. There are too many choices to begin with. On top of that,
+    there is the vocabulary that reads like English without making any sense.
+
+    The situation is unacceptable and it hurts FOSS. 
+
+    Palal aims to resolve that. It is a simple CLI utility that asks the user
+    a series of questions In-Plain-English and returns the best possible FOSS
+    license for his/her project.
+
+    Palal is inspired by the choosealicense.com and it uses the same license 
+    meta-data. However, while choosealicense.com displays the meta-data for the
+    given license, Palal returns the license for the given meta-data.
+
+    If you use Palal, I would love to have your feedback: 
+
+        https://github.com/alixedi/palal
+
+    Now *release* that code!
+
+    :copyright (c) 2015 by Ali Zaidi.
+    :license: BSD, see LICENSE for more details.
+
+    ~ * ~
+"""
+    
 import os
 import yaml
+import argparse
+
 
 import sys
 if sys.version[0]=="3": raw_input=input
@@ -11,6 +45,8 @@ RULES = os.path.join(DATA, 'rules.yml')
 LICENSES = os.path.join(DIR, '_licenses')
 YAML_SEP = '---'
 
+_desc = 'Answer a few questions to get the best FOSS license for your project.'
+parser = argparse.ArgumentParser(description=_desc)
 
 # Prettyprint utilities
 def print1(s):
@@ -94,6 +130,8 @@ def dget(d, k):
 
 
 def main():
+    # parse args
+    args = parser.parse_args()
     # load everything
     lics = load_licenses()
     rules = load_rules()
